eca64273fcd0938177232a0143e9fd9b968c5166
Jose Miguel Gamazo Sanchez <uha95@mundo-r.com>
2011-12-13 11:04:52 +0100

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Managebooks::Application.config.secret_token = 'f1229fab5a5efa53a496bb2e31521f86ca312dbe7b465821a0feaee731b82b1d0cdf87dabd1e2238b36fc22213b44a746414ce690eb09b995bc3b458b8e11a24'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Managebooks::Application.config.session_store :cookie_store, key: '_managebooks_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Managebooks::Application.config.session_store :active_record_store
