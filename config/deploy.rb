53a29d2edab0e2d2ad2d9bf79fdd84674dc97e15
Jose Miguel Gamazo Sanchez <uha95@mundo-r.com>
2011-12-13 12:01:30 +0100

    capistrano deploy file

---
+++ config/deploy.rb
@@ -0,0 +1,37 @@
+$:.unshift(File.expand_path('./lib', ENV['rvm_path']))
+require "rvm/capistrano"
+set :rvm_ruby_string, 'ruby-1.9.2-p290'
+set :rvm_type, :user
+require "bundler/capistrano"
+set :application, "reservas"
+set :domain, "173.203.94.31"
+set :user, "miguel"
+set :port, 53200
+set :repository,  "git@github.com:jgamsan/managebooks.git"
+default_run_options[:pty] = true
+
+set :scm, :git
+
+default_run_options[:pty] = true
+set :use_sudo, false
+set :deploy_via, :remote_cache
+set :deploy_to, "/home/#{user}/public_html/#{application}"
+role :web, domain
+role :app, domain
+role :db,  domain, :primary => true
+
+set :rails_env, "production"
+
+namespace :deploy do
+  task :start do
+    run "touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+  task :stop do end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
+after 'deploy:update_code' do
+  run "cd #{release_path}; RAILS_ENV=production bundle exec rake assets:precompile"
+end
+after "deploy", "deploy:cleanup"
