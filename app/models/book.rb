0e23e8c02040e9a5a712db87115949e563942bb5
Jose Miguel Gamazo Sanchez <uha95@mundo-r.com>
2011-12-18 21:36:32 +0100

    error in sql books for user

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,7 +3,6 @@ class Book < ActiveRecord::Base
   belongs_to :resort
   belongs_to :interval
   scope :busy, lambda { |day, resort| where(:resort_id => resort, :day => day) }
-  scope :by_user, lambda { |user| where{(user_id == user) & (day >= Date.today)}}
   scope :total, lambda { |day, resort, user| where{(user_id == user) & (day >= Date.today) & (resort_id == resort)}}
 
   class << self
@@ -15,6 +14,11 @@ class Book < ActiveRecord::Base
     def weekly(value, day)
       books = Book.busy(day, value)
       Interval.where{(id.not_in(books.select{interval_id})) & (resort_id == value)}
+    end
+
+    def by_user(usuario)
+      @user = User.find_by_uid(usuario)
+      @user.books.where(:user_id => @user.id, :day => "=> #{Date.today}")
     end
   end
 
