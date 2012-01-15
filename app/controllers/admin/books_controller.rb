d8bce2c9d40f7247e0b6597ea0714bf41d69e71d
Jose Miguel Gamazo Sanchez <jose@jose-desktop.(none)>
2012-01-15 21:24:58 +0100

    corregidos errores in controllers with resources

--- app/controllers/admin/books_controller.rb
+++ app/controllers/admin/books_controller.rb
@@ -1,7 +1,5 @@
-class Admin::BooksController < Admin::ResourcesController
+class Admin::BooksController < Admin::BaseController
   def index
-    @intervals = Interval.list_by_store_admin(admin_user.id)
-    params.merge!({:interval_id => @intervals}) if admin_user.role == "storeadmin"
-    super
+   
   end
 end
--- app/controllers/admin/offers_controller.rb
+++ app/controllers/admin/offers_controller.rb
@@ -1,7 +1,5 @@
-class Admin::OffersController < Admin::ResourcesController
+class Admin::OffersController < Admin::BaseController
   def index
-    @resorts = Resort.by_storeadmin(admin_user.id).map {|x| x.id}.flatten
-    params.merge!({:resort_id => @resorts}) if admin_user.role == "storeadmin"
-    super
+    
   end
 end
