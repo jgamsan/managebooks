017b2dc91a8bb692a08628d81026792281d186a3
Jose Miguel Gamazo Sanchez <jose@jose-desktop.(none)>
2012-02-10 22:33:34 +0100

    Mejoras en mailer

--- app/mailers/admins.rb
+++
@@ -1,8 +0,0 @@
-class Admin::Admins < ActionMailer::Base
-  default from: "no-reply@webreserva.me"
-  
-  def reset_password_instructions(user)
-    @admin = user
-    mail(:to => user.email, :subject => "Bienvenido al Panel de Control de Webreserva")
-  end
-end
