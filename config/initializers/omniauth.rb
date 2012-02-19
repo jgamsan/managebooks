a23d36d4bd7932d8f075bee3b7377de061a658cc
Jose Miguel <miguel.gamazo@galiclick.com>
2012-02-19 21:33:58 +0100

    add more providers for omniauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,7 +3,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, '468598197517.apps.googleusercontent.com', '2G6JJeIVACEyzaWjtJtsHW9g'
   provider :facebook, '275260129193102', '9a59059cb24974d5468a59be2ef0408d'
   provider :github, '6a5d0383976fdc103f6e', '15cce3946f10169c8776ba66d327b7ca7cd4f245'
-  provider :windowslive, '000000004C08183C', 'g2p04ei4eo1YhiWIB7k3DEK913qkb08A'
+  provider :windowslive, '000000004C08183C', 'g2p04ei4eo1YhiWIB7k3DEK913qkb08A', :scope => 'wl.basic'
   provider :linkedin, '73r1451vche8', 'aGzJOQvhFwabrQ9A'
   provider :identity, on_failed_registration: lambda { |env|
     IdentitiesController.action(:new).call(env)
